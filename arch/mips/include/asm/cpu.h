RonGokhale/codeaurora_kernel

MIPS: cpu: Add new cpu option for Hardware Table Walker.
Moreover, report hardware page table walker support as 'htw' in the ASE
list of /proc/cpuinfo, if the core implements this feature.

Signed-off-by: Markos Chandras <markos.chandras@imgtec.com>
Cc: linux-mips@linux-mips.org
Patchwork: https://patchwork.linux-mips.org/patch/7334/
Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

arch/mips/include/asm/cpu.h
@@ -365,6 +365,7 @@ enum cpu_type_enum {
 #define MIPS_CPU_TLBINV		0x02000000ull /* CPU supports TLBINV/F */
 #define MIPS_CPU_SEGMENTS	0x04000000ull /* CPU supports Segmentation Control registers */
 #define MIPS_CPU_EVA		0x80000000ull /* CPU supports Enhanced Virtual Addressing */
+#define MIPS_CPU_HTW		0x100000000ull /* CPU support Hardware Page Table Walker */
 
 /*
  * CPU ASE encodings

FlyGoat/linux-stable

Signed-off-by: Huacai Chen <chenhc@lemote.com>

arch/mips/include/asm/cpu.h
@@ -328,6 +328,7 @@ enum cpu_type_enum {
 #define MIPS_CPU_RIXI		0x00800000ull /* CPU has TLB Read/eXec Inhibit */
 #define MIPS_CPU_MICROMIPS	0x01000000ull /* CPU has microMIPS capability */
 #define MIPS_CPU_TLBINV		0x02000000ull /* CPU supports TLBINV/F */
+#define MIPS_CPU_HTW		0x040000000ull /* CPU support Hardware Page Table Walker */
 
 /*
  * CPU ASE encodings