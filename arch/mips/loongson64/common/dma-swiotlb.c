lshw/linux-loongson_lemote

Loongson: Add basic support for LS2H bridge
Signed-off-by: Huacai Chen <chenhc@lemote.com>

arch/mips/loongson64/common/dma-swiotlb.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <linux/mm.h>
 #include <linux/init.h>
+#include <linux/sizes.h>
 #include <linux/dma-mapping.h>
 #include <linux/scatterlist.h>
 #include <linux/swiotlb.h>
@@ -8,6 +9,7 @@
 
 #include <asm/bootinfo.h>
 #include <boot_param.h>
+#include <loongson-pch.h>
 #include <dma-coherence.h>
 
 static inline void *dma_to_virt(struct device *dev, dma_addr_t dma_addr)
@@ -31,7 +33,7 @@ static void *loongson_dma_alloc_coherent(struct device *dev, size_t size,
 		 dev->coherent_dma_mask < DMA_BIT_MASK(40))
 		gfp |= __GFP_DMA32;
 
-	gfp |= __GFP_NORETRY;
+	gfp |= __GFP_NORETRY|__GFP_NOWARN;
 
 	ret = swiotlb_alloc_coherent(dev, size, dma_handle, gfp);
 	if (!plat_device_is_coherent(dev)) {
@@ -80,13 +82,19 @@ static int loongson_dma_mmap(struct device *dev, struct vm_area_struct *vma,
 	return ret;
 }
 
+#define PCIE_DMA_ALIGN 16
+
 static dma_addr_t loongson_dma_map_page(struct device *dev, struct page *page,
 				unsigned long offset, size_t size,
 				enum dma_data_direction dir,
 				unsigned long attrs)
 {
-	dma_addr_t daddr = swiotlb_map_page(dev, page, offset, size,
-					dir, attrs);
+	dma_addr_t daddr;
+
+	if (offset % PCIE_DMA_ALIGN)
+		attrs |= dev->archdata.dma_attrs;
+
+	daddr = swiotlb_map_page(dev, page, offset, size, dir, attrs);
 	if (!plat_device_is_coherent(dev))
 		dma_cache_sync(dev, dma_to_virt(dev, daddr), size, dir);
 	mb();
@@ -110,6 +118,7 @@ static int loongson_dma_map_sg(struct device *dev, struct scatterlist *sgl,
 	int i, r;
 	struct scatterlist *sg;
 
+	attrs |= dev->archdata.dma_attrs;
 	r = swiotlb_map_sg_attrs(dev, sgl, nents, dir, attrs);
 	if (!plat_device_is_coherent(dev)) {
 		for_each_sg(sgl, sg, nents, i)
@@ -193,7 +202,25 @@ static int loongson_dma_supported(struct device *dev, u64 mask)
 	return swiotlb_dma_supported(dev, mask);
 }
 
-dma_addr_t phys_to_dma(struct device *dev, phys_addr_t paddr)
+#define SZ_4G	0x100000000ULL
+
+static dma_addr_t loongson_ls2h_phys_to_dma(struct device *dev, phys_addr_t paddr)
+{
+	dma_addr_t daddr;
+
+	daddr = (paddr < SZ_256M) ? paddr :
+		(paddr - loongson_sysconf.high_physmem_start + SZ_256M);
+
+	return (daddr < SZ_4G) ? daddr : -1ULL; /* DMA address should be below 4GB */
+}
+
+static phys_addr_t loongson_ls2h_dma_to_phys(struct device *dev, dma_addr_t daddr)
+{
+	return (daddr < SZ_256M) ? daddr :
+		(daddr + loongson_sysconf.high_physmem_start - SZ_256M);
+}
+
+static dma_addr_t loongson_rs780_phys_to_dma(struct device *dev, phys_addr_t paddr)
 {
 	long nid;
 #ifdef CONFIG_PHYS48_TO_HT40
@@ -205,7 +232,7 @@ dma_addr_t phys_to_dma(struct device *dev, phys_addr_t paddr)
 	return paddr;
 }
 
-phys_addr_t dma_to_phys(struct device *dev, dma_addr_t daddr)
+static phys_addr_t loongson_rs780_dma_to_phys(struct device *dev, dma_addr_t daddr)
 {
 	long nid;
 #ifdef CONFIG_PHYS48_TO_HT40
@@ -217,25 +244,56 @@ phys_addr_t dma_to_phys(struct device *dev, dma_addr_t daddr)
 	return daddr;
 }
 
-static const struct dma_map_ops loongson_dma_map_ops = {
-	.alloc = loongson_dma_alloc_coherent,
-	.free = loongson_dma_free_coherent,
-	.mmap = loongson_dma_mmap,
-	.map_page = loongson_dma_map_page,
-	.unmap_page = loongson_dma_unmap_page,
-	.map_sg = loongson_dma_map_sg,
-	.unmap_sg = loongson_dma_unmap_sg,
-	.sync_single_for_cpu = loongson_dma_sync_single_for_cpu,
-	.sync_single_for_device = loongson_dma_sync_single_for_device,
-	.sync_sg_for_cpu = loongson_dma_sync_sg_for_cpu,
-	.sync_sg_for_device = loongson_dma_sync_sg_for_device,
-	.mapping_error = swiotlb_dma_mapping_error,
-	.dma_supported = loongson_dma_supported,
-	.get_cache_alignment = mips_dma_get_cache_alignment
+struct loongson_dma_map_ops {
+	struct dma_map_ops dma_map_ops;
+	dma_addr_t (*phys_to_dma)(struct device *dev, phys_addr_t paddr);
+	phys_addr_t (*dma_to_phys)(struct device *dev, dma_addr_t daddr);
+};
+
+dma_addr_t phys_to_dma(struct device *dev, phys_addr_t paddr)
+{
+	struct loongson_dma_map_ops *ops = container_of(get_dma_ops(dev),
+					struct loongson_dma_map_ops, dma_map_ops);
+
+	return ops->phys_to_dma(dev, paddr);
+}
+
+phys_addr_t dma_to_phys(struct device *dev, dma_addr_t daddr)
+{
+	struct loongson_dma_map_ops *ops = container_of(get_dma_ops(dev),
+					struct loongson_dma_map_ops, dma_map_ops);
+
+	return ops->dma_to_phys(dev, daddr);
+}
+
+static struct loongson_dma_map_ops loongson_linear_dma_map_ops = {
+	.dma_map_ops = {
+		.alloc = loongson_dma_alloc_coherent,
+		.free = loongson_dma_free_coherent,
+		.mmap = loongson_dma_mmap,
+		.map_page = loongson_dma_map_page,
+		.unmap_page = loongson_dma_unmap_page,
+		.map_sg = loongson_dma_map_sg,
+		.unmap_sg = loongson_dma_unmap_sg,
+		.sync_single_for_cpu = loongson_dma_sync_single_for_cpu,
+		.sync_single_for_device = loongson_dma_sync_single_for_device,
+		.sync_sg_for_cpu = loongson_dma_sync_sg_for_cpu,
+		.sync_sg_for_device = loongson_dma_sync_sg_for_device,
+		.mapping_error = swiotlb_dma_mapping_error,
+		.dma_supported = loongson_dma_supported,
+		.get_cache_alignment = mips_dma_get_cache_alignment
+	},
+	.phys_to_dma = loongson_rs780_phys_to_dma,
+	.dma_to_phys = loongson_rs780_dma_to_phys
 };
 
 void __init plat_swiotlb_setup(void)
 {
 	swiotlb_init(1);
-	mips_dma_map_ops = &loongson_dma_map_ops;
+	mips_dma_map_ops = &loongson_linear_dma_map_ops.dma_map_ops;
+
+	if (loongson_pch && loongson_pch->board_type == LS2H) {
+		loongson_linear_dma_map_ops.phys_to_dma = loongson_ls2h_phys_to_dma;
+		loongson_linear_dma_map_ops.dma_to_phys = loongson_ls2h_dma_to_phys;
+	}
 }

FlyGoat/linux-stable

arch/mips/loongson64/common/dma-swiotlb.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <linux/mm.h>
 #include <linux/init.h>
+#include <linux/sizes.h>
 #include <linux/dma-mapping.h>
 #include <linux/scatterlist.h>
 #include <linux/swiotlb.h>
@@ -8,6 +9,7 @@
 
 #include <asm/bootinfo.h>
 #include <boot_param.h>
+#include <loongson-pch.h>
 #include <dma-coherence.h>
 
 static inline void *dma_to_virt(struct device *dev, dma_addr_t dma_addr)
@@ -66,13 +68,19 @@ static int loongson_dma_mmap(struct device *dev, struct vm_area_struct *vma,
 	return ret;
 }
 
+#define PCIE_DMA_ALIGN 16
+
 static dma_addr_t loongson_dma_map_page(struct device *dev, struct page *page,
 				unsigned long offset, size_t size,
 				enum dma_data_direction dir,
 				unsigned long attrs)
 {
-	dma_addr_t daddr = swiotlb_map_page(dev, page, offset, size,
-					dir, attrs);
+	dma_addr_t daddr;
+
+	if (offset % PCIE_DMA_ALIGN)
+		attrs |= dev->archdata.dma_attrs;
+
+	daddr = swiotlb_map_page(dev, page, offset, size, dir, attrs);
 	if (!plat_device_is_coherent(dev) && !(attrs & DMA_ATTR_SKIP_CPU_SYNC))
 		__dma_sync_virtual(dma_to_virt(dev, daddr), size, dir);
 	mb();
@@ -96,6 +104,7 @@ static int loongson_dma_map_sg(struct device *dev, struct scatterlist *sgl,
 	int i, r;
 	struct scatterlist *sg;
 
+	attrs |= dev->archdata.dma_attrs;
 	r = swiotlb_map_sg_attrs(dev, sgl, nents, dir, attrs);
 	if (!plat_device_is_coherent(dev) && !(attrs & DMA_ATTR_SKIP_CPU_SYNC)) {
 		for_each_sg(sgl, sg, nents, i)
@@ -179,7 +188,23 @@ static int loongson_dma_supported(struct device *dev, u64 mask)
 	return swiotlb_dma_supported(dev, mask);
 }
 
-dma_addr_t __phys_to_dma(struct device *dev, phys_addr_t paddr)
+static dma_addr_t loongson_ls2h_phys_to_dma(struct device *dev, phys_addr_t paddr)
+{
+	dma_addr_t daddr;
+
+	daddr = (paddr < SZ_256M) ? paddr :
+		(paddr - loongson_sysconf.high_physmem_start + SZ_256M);
+
+	return (daddr < SZ_4G) ? daddr : -1ULL; /* DMA address should be below 4GB */
+}
+
+static phys_addr_t loongson_ls2h_dma_to_phys(struct device *dev, dma_addr_t daddr)
+{
+	return (daddr < SZ_256M) ? daddr :
+		(daddr + loongson_sysconf.high_physmem_start - SZ_256M);
+}
+
+static dma_addr_t loongson_rs780_phys_to_dma(struct device *dev, phys_addr_t paddr)
 {
 	long nid;
 #ifdef CONFIG_PHYS48_TO_HT40
@@ -191,7 +216,7 @@ dma_addr_t __phys_to_dma(struct device *dev, phys_addr_t paddr)
 	return paddr;
 }
 
-phys_addr_t __dma_to_phys(struct device *dev, dma_addr_t daddr)
+static phys_addr_t loongson_rs780_dma_to_phys(struct device *dev, dma_addr_t daddr)
 {
 	long nid;
 #ifdef CONFIG_PHYS48_TO_HT40
@@ -203,25 +228,60 @@ phys_addr_t __dma_to_phys(struct device *dev, dma_addr_t daddr)
 	return daddr;
 }
 
-static const struct dma_map_ops loongson_dma_map_ops = {
-	.alloc = loongson_dma_alloc_coherent,
-	.free = loongson_dma_free_coherent,
-	.mmap = loongson_dma_mmap,
-	.map_page = loongson_dma_map_page,
-	.unmap_page = loongson_dma_unmap_page,
-	.map_sg = loongson_dma_map_sg,
-	.unmap_sg = loongson_dma_unmap_sg,
-	.sync_single_for_cpu = loongson_dma_sync_single_for_cpu,
-	.sync_single_for_device = loongson_dma_sync_single_for_device,
-	.sync_sg_for_cpu = loongson_dma_sync_sg_for_cpu,
-	.sync_sg_for_device = loongson_dma_sync_sg_for_device,
-	.mapping_error = swiotlb_dma_mapping_error,
-	.dma_supported = loongson_dma_supported,
-	.get_cache_alignment = mips_dma_get_cache_alignment
+struct loongson_dma_map_ops {
+	struct dma_map_ops dma_map_ops;
+	dma_addr_t (*phys_to_dma)(struct device *dev, phys_addr_t paddr);
+	phys_addr_t (*dma_to_phys)(struct device *dev, dma_addr_t daddr);
+};
+
+dma_addr_t __phys_to_dma(struct device *dev, phys_addr_t paddr)
+{
+	struct loongson_dma_map_ops *ops = container_of(get_dma_ops(dev),
+					struct loongson_dma_map_ops, dma_map_ops);
+
+	return ops->phys_to_dma(dev, paddr);
+}
+
+phys_addr_t __dma_to_phys(struct device *dev, dma_addr_t daddr)
+{
+	struct loongson_dma_map_ops *ops = container_of(get_dma_ops(dev),
+					struct loongson_dma_map_ops, dma_map_ops);
+
+	return ops->dma_to_phys(dev, daddr);
+}
+
+static struct loongson_dma_map_ops loongson_linear_dma_map_ops = {
+	.dma_map_ops = {
+		.alloc = loongson_dma_alloc_coherent,
+		.free = loongson_dma_free_coherent,
+		.mmap = loongson_dma_mmap,
+		.map_page = loongson_dma_map_page,
+		.unmap_page = loongson_dma_unmap_page,
+		.map_sg = loongson_dma_map_sg,
+		.unmap_sg = loongson_dma_unmap_sg,
+		.sync_single_for_cpu = loongson_dma_sync_single_for_cpu,
+		.sync_single_for_device = loongson_dma_sync_single_for_device,
+		.sync_sg_for_cpu = loongson_dma_sync_sg_for_cpu,
+		.sync_sg_for_device = loongson_dma_sync_sg_for_device,
+		.mapping_error = swiotlb_dma_mapping_error,
+		.dma_supported = loongson_dma_supported,
+		.get_cache_alignment = mips_dma_get_cache_alignment
+	},
 };
 
 void __init plat_swiotlb_setup(void)
 {
 	swiotlb_init(1);
-	mips_dma_map_ops = &loongson_dma_map_ops;
+	mips_dma_map_ops = &loongson_linear_dma_map_ops.dma_map_ops;
+
+	switch (loongson_pch->type) {
+	case LS2H:
+		loongson_linear_dma_map_ops.phys_to_dma = loongson_ls2h_phys_to_dma;
+		loongson_linear_dma_map_ops.dma_to_phys = loongson_ls2h_dma_to_phys;
+		break;
+	case RS780E:
+		loongson_linear_dma_map_ops.phys_to_dma = loongson_rs780_phys_to_dma;
+		loongson_linear_dma_map_ops.dma_to_phys = loongson_rs780_dma_to_phys;
+		break;
+	}
 }